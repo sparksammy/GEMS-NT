cf7c1029f7ed49dc63ee1bb95f4b39d6bf38bac9
Samuel Lord <sllord@icloud.com>
2020-08-09 09:36:47 -0500

    Oops! forgot to release source again!
    frick.

--- GEMSNT/Kernel.cs
+++ GEMSNT/Kernel.cs
@@ -8,7 +8,7 @@ namespace GEMSNT
 {
     public class Kernel : Sys.Kernel
     {
-        string versionSTR = "0.41a";
+        string versionSTR = "0.43a";
 
         Sys.FileSystem.CosmosVFS fs;
         string current_directory = "0:\\";
@@ -32,7 +32,11 @@ namespace GEMSNT
                 if (cmd == "clear") {
                     Console.Clear();
                 } else if (cmd == "about") {
+                    long available_space = fs.GetAvailableFreeSpace("0:/");
+                    string fs_type = fs.GetFileSystemType("0:/");
                     Console.WriteLine("GEMS NT Version: " + versionSTR);
+                    Console.WriteLine("File System Type on main drive: " + fs_type);
+                    Console.WriteLine("Available Free Space on main drive: " + available_space);
                 } else if (cmd == "dir") {
                     string[] dirs = GetDirFadr(current_directory);
                     foreach (var item in dirs)
@@ -48,11 +52,45 @@ namespace GEMSNT
                 } else if (cmd.ToString().Contains("del")) {
                     var delFile = cmd.ToString().Remove(0, 4);
                     File.Delete(delFile);
-                } else if (cmd.ToString().Contains("echo2file")) {
-                    var writeContents = cmd.ToString().Remove(0, 10);
-                    Console.Write("File Name> ");
+                } else if (cmd.ToString().Contains("2file")) {
+                    var writeContents = cmd.ToString().Remove(0, 6);
+                    Console.Write("File Path> ");
                     var echoFile = Console.ReadLine();
-                    System.IO.File.WriteAllText(echoFile, writeContents);
+                    try
+                    {
+                        var hello_file = fs.GetFile(echoFile);
+                        var hello_file_stream = hello_file.GetFileStream();
+
+                        if (hello_file_stream.CanWrite)
+                        {
+                            byte[] text_to_write = Encoding.ASCII.GetBytes(writeContents);
+                            hello_file_stream.Write(text_to_write, 0, text_to_write.Length);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                } else if (cmd.ToString().Contains("mkdir")) {
+                    var makeDirPath = cmd.ToString().Remove(0, 6);
+                    try
+                    {
+                        fs.CreateDirectory(makeDirPath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                } else if (cmd.ToString().Contains("mkfile")) {
+                    var makeFilePath = cmd.ToString().Remove(0, 7);
+                    try
+                    {
+                        fs.CreateFile(makeFilePath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
                 } else if (cmd.ToString().Contains("dog")) {
                     var dogFile = cmd.ToString().Remove(0, 4);
                     try
@@ -76,7 +114,9 @@ namespace GEMSNT
                     Console.WriteLine("---");
                     Console.WriteLine("about - about this copy of GEMS.");
                     Console.WriteLine("echo - echos back value you pass to it");
-                    Console.WriteLine("echo2file - writes a value to a file.");
+                    Console.WriteLine("2file - writes a specified value to a file.");
+                    Console.WriteLine("mkfile - creates a file at specified directory");
+                    Console.WriteLine("mkdir - creates the specified directory");
                     Console.WriteLine("del - deletes a file");
                     Console.WriteLine("cd - changes directory to passed directory");
                     Console.WriteLine("dir - list contents of directory");

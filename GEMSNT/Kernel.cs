325fb38d062b3a5f2f0d44c44f478aa4feec7186
Samuel Lord <sllord@icloud.com>
2020-08-09 20:21:46 -0500

    0.5prebeta source

--- GEMSNT/Kernel.cs
+++ GEMSNT/Kernel.cs
@@ -8,10 +8,12 @@ namespace GEMSNT
 {
     public class Kernel : Sys.Kernel
     {
-        string versionSTR = "0.43a";
+        string versionSTR = "0.5prebeta";
 
         Sys.FileSystem.CosmosVFS fs;
+
         string current_directory = "0:\\";
+        string currDir = "";
 
         protected override void BeforeRun()
         {
@@ -23,6 +25,7 @@ namespace GEMSNT
 
         protected override void Run()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             while (1 == 1)
             {
                 Console.Write(current_directory + " $> ");
@@ -35,67 +38,125 @@ namespace GEMSNT
                     long available_space = fs.GetAvailableFreeSpace("0:/");
                     string fs_type = fs.GetFileSystemType("0:/");
                     Console.WriteLine("GEMS NT Version: " + versionSTR);
-                    Console.WriteLine("File System Type on main drive: " + fs_type);
-                    Console.WriteLine("Available Free Space on main drive: " + available_space);
+                    if (fs_type != null)
+                    {
+                        Console.WriteLine("File System Type on main drive: " + fs_type);
+                        Console.WriteLine("Available Free Space on main drive: " + available_space);
+                    } else
+                    {
+                        Console.WriteLine("File System Type on main drive: NONE");
+                        Console.WriteLine("Available Free Space on main drive: 0");
+                    }
+                } else if (cmd == "rainbowConnection") {
+                    Console.WriteLine("Why are there so many songs about rainbows,");
+                    Console.WriteLine("and what's on the other side?");
+                    Console.WriteLine("--Kermit");
+                    Console.Beep(700, 400);
+                    Console.Beep(894, 400);
+                    Console.Beep(990, 600);
+                    Console.Beep(950, 400);
+                    Console.Beep(900, 400);
+                    Console.Beep(1000, 600);
+                    Console.Beep(666, 800);
+                    Console.Beep(700, 600);
+                    Console.Beep(861, 600);
+                    Console.Beep(925, 600);
+                    Console.Beep(950, 400);
+                    Console.Beep(975, 600);
+                    Console.Beep(942, 400);
+                    Console.Beep(875, 400);
+                    Console.Beep(800, 600);
+                    Console.Beep(942, 400);
+                    Console.Beep(990, 600);
+                    Console.Beep(800, 600);
                 } else if (cmd == "dir") {
-                    string[] dirs = GetDirFadr(current_directory);
+                    string[] dirs = Directory.GetDirectories(current_directory);
+                    string[] files = Directory.GetFiles(current_directory);
+                    Console.WriteLine("Dirs:");
+                    Console.WriteLine("---");
                     foreach (var item in dirs)
                     {
                         Console.WriteLine(item);
                     }
-                } else if (cmd.ToString().Contains("echo")) {
+                    Console.WriteLine("---");
+                    Console.WriteLine("Files:");
+                    Console.WriteLine("---");
+                    foreach (var item in files)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("---");
+                } else if (cmd.ToString().StartsWith("echo")) {
                     var echoing = cmd.ToString().Remove(0, 5);
                     Console.WriteLine(echoing);
-                } else if (cmd.ToString().Contains("cd")) {
+                } else if (cmd.ToString().StartsWith("cd")) {
                     var dirCD = cmd.ToString().Remove(0, 3);
-                    Directory.SetCurrentDirectory(dirCD);
-                } else if (cmd.ToString().Contains("del")) {
+                    if (dirCD == "..") {
+                        string prevDirTemp = current_directory.ToString().Replace(currDir.ToString() + "\\", "\\");
+                        Console.WriteLine(prevDirTemp.ToString());
+                        current_directory = prevDirTemp.ToString();
+                        currDir = fs.GetDirectory(prevDirTemp).mName.ToString();
+                    } else {
+                        currDir = dirCD.ToString();
+                        if (fs.GetDirectory(current_directory + dirCD) != null)
+                        {
+                            current_directory = current_directory + dirCD + "\\";
+                        }
+                        else
+                        {
+                            Console.WriteLine("!!! Directory not found. !!!");
+                        }
+                    }
+                } else if (cmd.ToString().StartsWith("del")) {
                     var delFile = cmd.ToString().Remove(0, 4);
                     File.Delete(delFile);
-                } else if (cmd.ToString().Contains("2file")) {
+                } else if (cmd.ToString().StartsWith("ddir")) {
+                    var delDir = cmd.ToString().Remove(0, 5);
+                    try {
+                        fs.DeleteDirectory(fs.GetDirectory(delDir));
+                    } catch (Exception e) {
+                        Console.WriteLine(e.ToString());
+                    }
+                } else if (cmd.ToString().StartsWith("2file")) {
                     var writeContents = cmd.ToString().Remove(0, 6);
-                    Console.Write("File Path> ");
+                    Console.Write("Filename> ");
                     var echoFile = Console.ReadLine();
                     try
                     {
-                        var hello_file = fs.GetFile(echoFile);
-                        var hello_file_stream = hello_file.GetFileStream();
-
-                        if (hello_file_stream.CanWrite)
+                        using (StreamWriter w = File.AppendText(current_directory + echoFile))
                         {
-                            byte[] text_to_write = Encoding.ASCII.GetBytes(writeContents);
-                            hello_file_stream.Write(text_to_write, 0, text_to_write.Length);
+                            w.WriteLine(writeContents);
                         }
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e.ToString());
                     }
-                } else if (cmd.ToString().Contains("mkdir")) {
-                    var makeDirPath = cmd.ToString().Remove(0, 6);
+                } else if (cmd.ToString().StartsWith("mkdir")) {
+                    var makeDir = cmd.ToString().Remove(0, 6);
                     try
                     {
-                        fs.CreateDirectory(makeDirPath);
+                        fs.CreateDirectory(makeDir);
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e.ToString());
                     }
-                } else if (cmd.ToString().Contains("mkfile")) {
-                    var makeFilePath = cmd.ToString().Remove(0, 7);
+                } else if (cmd.ToString().StartsWith("mkfile")) {
+                    var makeFile = cmd.ToString().Remove(0, 7);
                     try
                     {
-                        fs.CreateFile(makeFilePath);
+                        File.Create(current_directory + makeFile);
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e.ToString());
                     }
-                } else if (cmd.ToString().Contains("dog")) {
+                } else if (cmd.ToString().StartsWith("dog")) {
                     var dogFile = cmd.ToString().Remove(0, 4);
                     try
                     {
-                        var hello_file = fs.GetFile(dogFile);
+                        var hello_file = fs.GetFile(current_directory + dogFile);
                         var hello_file_stream = hello_file.GetFileStream();
 
                         if (hello_file_stream.CanRead)
@@ -113,11 +174,13 @@ namespace GEMSNT
                     Console.WriteLine("GEMS NT - Commands");
                     Console.WriteLine("---");
                     Console.WriteLine("about - about this copy of GEMS.");
+                    Console.WriteLine("rainbowConnection - Muppets reference");
                     Console.WriteLine("echo - echos back value you pass to it");
-                    Console.WriteLine("2file - writes a specified value to a file.");
-                    Console.WriteLine("mkfile - creates a file at specified directory");
+                    Console.WriteLine("2file - writes a specified value to a file.");  //renable once file creation is back.
+                    Console.WriteLine("mkfile - creates a file at specified directory"); //renable once file creation is back.
                     Console.WriteLine("mkdir - creates the specified directory");
-                    Console.WriteLine("del - deletes a file");
+                    Console.WriteLine("del - deletes a file, NOT DIRECTORIES.");  //renable once file creation is back.
+                    //Console.WriteLine("ddir - deletes a directory, NOT FILES.");
                     Console.WriteLine("cd - changes directory to passed directory");
                     Console.WriteLine("dir - list contents of directory");
                     Console.WriteLine("clear - clears screen");

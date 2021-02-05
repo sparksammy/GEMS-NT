655cb2f003c0b8d4a26ded28040b11e43612e244
Samuel Lord <sllord@icloud.com>
2021-02-05 05:33:30 +0000

    Update macro.cs

--- GEMSNT/macro.cs
+++ GEMSNT/macro.cs
@@ -1,113 +1,132 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.IO;
-using Sys = Cosmos.System;
 
-namespace GEMSNT
+namespace Micro4Win
 {
-
-
-    public class Micro
+    class Program
     {
-        static string contents = "";
-        static string path = "";
-        public static void welcome()
+        public class Micro
         {
-            Console.Clear();
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.WriteLine("~                             micro - GEMS Text Editor");
-            Console.WriteLine("~                      A simple UI for 2file, inspired by MIV.");
-            Console.WriteLine("~                                  version 1.0");
-            Console.WriteLine("~                                 by Samuel Lord");
-            Console.WriteLine("~                                 License - MIT");
-            Console.WriteLine("~");
-            Console.WriteLine("~                      use q<enter>            to exit");
-            Console.WriteLine("~                      use w<enter>            save to file");
-            Console.WriteLine("~                      use c<enter>            to clear");
-            Console.WriteLine("~                   type :$m<enter>            to use commands");
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.WriteLine("~");
-            Console.Write("~                                 Press any key to continue...");
-            Console.ReadKey(true); //any key to continue
-            Console.Clear();
-            micro();
-
-
-        }
-
-        public static void startMicro(string pathb)
-        {
-            if (File.Exists(pathb))
+            public static string contents = "";
+            public static void welcome(string path)
             {
-                microLoader(pathb);
-                welcome();
-            }
-            else
-            {
-                welcome();
-            }
-        }
+                Console.Clear();
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.WriteLine("~                             micro - GEMS Text Editor");
+                Console.WriteLine("~                           A simple MIV-like text editor.");
+                Console.WriteLine("~                                  version 1.0");
+                Console.WriteLine("~                                 by Samuel Lord");
+                Console.WriteLine("~                                 License - MIT");
+                Console.WriteLine("~");
+                Console.WriteLine("~                      use q<enter>             to exit");
+                Console.WriteLine("~                      use w<enter>            save to file");
+                Console.WriteLine("~                     press <escape>             use commands");
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.WriteLine("~");
+                Console.Write("~                                 Press any key to continue...");
+                Console.ReadKey(true); //any key to continue
+                Console.Clear();
+                micro(path);
 
-        public static void microLoader(string pathb)
-        {
-            //here we will open files and load them into the contents string.
-            try
-            {
-                var hello_text = File.ReadAllText(pathb);
-                contents = hello_text;
-                path = pathb;
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
 
-        public static String micro()
-        {
-            var cmdMode = false;
-            Console.Write("<**BEGIN READONLY**>");
-            Console.Write(contents);
-            Console.Write("<**END READONLY**>");
-            while (true)
-            { 
-                if (cmdMode == true)
+            public static void startMicro(string pathb)
+            {
+                if (File.Exists(pathb))
                 {
-                    Console.Write("m>");
-                    var cmd = Console.ReadLine();
-                    if (cmd == "c") {
-                        contents = "";
-                    } else if (cmd == "q")
+                    microLoader(pathb);
+                }
+                else
+                {
+                    welcome(pathb);
+                }
+            }
+
+            public static void microLoader(string pathb)
+            {
+                //here we will open files and load them into the contents string.
+                try
+                {
+                    var hello_text = File.ReadAllText(pathb);
+                    contents = hello_text;
+                    Console.WriteLine(hello_text);
+                    micro(pathb);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            public static String micro(string path)
+            {
+                while (true)
+                {
+                    var keyInf = Console.ReadKey(true);
+                    var currentKey = keyInf.Key;
+                    var editmode = true;
+                    var posLeftR = 0;
+                    var posTopB = 0;
+                    if (currentKey == ConsoleKey.Escape)
                     {
-                        return null;
-                    } else if (cmd == "s")
-                    {
-                        File.WriteAllText(path, contents);
+                        editmode = false;
+                        Console.Clear();
+                        Console.Write("CMD>");
+                        var inputCMD = Console.ReadLine();
+                        if (inputCMD == "q")
+                        {
+                            //quit
+                            Environment.Exit(0);
+                        }
+                        else if (inputCMD == "w")
+                        {
+                            //write the contents to the file
+                            File.WriteAllText(path, contents);
+                            editmode = true;
+                        }
+                        else
+                        {
+                            //do nothing
+                        }
+                        Console.Clear();
+                        Console.WriteLine(contents);
                     }
-                    cmdMode = false;
+                    editmode = true;
+                    if (editmode == true) {
+                        if (keyInf.Key == ConsoleKey.Enter) {
+                            Console.Write(Environment.NewLine);
+                            posTopB += 1;
+                            contents = contents + Environment.NewLine;
+                        } else if (keyInf.Key == ConsoleKey.Backspace) {
+                            contents = string.Concat(contents.SkipLast(3));
+                            Console.Clear();
+                            Console.WriteLine(contents);
+                        } else {
+                            Console.Write(keyInf.KeyChar);
+                            posLeftR += 1;
+                            contents = contents + keyInf.KeyChar;
+                        }
+                    }
                 }
-                var newline = Console.ReadLine();
-
-                if (newline == ":$m")
-                {
-                    cmdMode = true;
-                }
-
-                if (cmdMode == false)
-                {
-                    contents += "\n" + newline;
-                }
+                return "micro=Nano+Vi"; //hacky fix to earlier problem, just in case it returns.
+            }
+            static void Main(string[] args)
+            {
+                Console.Write("File name:");
+                var fPath = Console.ReadLine();
+                startMicro(fPath);
             }
         }
-
-
-
-
     }
 }
+
+
